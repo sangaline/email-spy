cbb51cab9d31c9b50c12587c0a93954d1217de52
Evan Sangaline <esangaline@gmail.com>
2017-04-27 09:58:34 -0700

    Add the basic skeleton of the project.

---
+++ src/js/popup.js
@@ -0,0 +1,9 @@
+import "../css/popup.css";
+import Placeholder from "./popup/placeholder.jsx";
+import React from "react";
+import { render } from "react-dom";
+
+render(
+  <Placeholder/>,
+  window.document.getElementById("app")
+);
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+var webpack = require("webpack"),
+    path = require("path"),
+    env = require("./utils/env"),
+    HtmlWebpackPlugin = require("html-webpack-plugin"),
+    WriteFilePlugin = require("write-file-webpack-plugin");
+
+var options = {
+  entry: {
+    popup: path.join(__dirname, "src", "js", "popup.js"),
+  },
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: "[name].bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        loader: "style-loader!css-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".css"],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify(env.NODE_ENV)
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "popup.html"),
+      filename: "popup.html",
+      chunks: ["popup"]
+    }),
+    new WriteFilePlugin(),
+  ]
+};
+
+if (env.NODE_ENV === "development") {
+  options.devtool = "cheap-module-eval-source-map";
+}
+
+module.exports = options;
