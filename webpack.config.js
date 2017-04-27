5b834bafface214ff77be985082bafb43259337b
Evan Sangaline <esangaline@gmail.com>
2017-04-27 10:36:01 -0700

    Update the webpack config to pass linting.

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,55 @@
-var webpack = require("webpack"),
-    path = require("path"),
-    env = require("./utils/env"),
-    HtmlWebpackPlugin = require("html-webpack-plugin"),
-    WriteFilePlugin = require("write-file-webpack-plugin");
+const webpack = require('webpack');
+const path = require('path');
+const env = require('./utils/env');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WriteFilePlugin = require('write-file-webpack-plugin');
 
-var options = {
+const options = {
   entry: {
-    popup: path.join(__dirname, "src", "js", "popup.js"),
+    popup: path.join(__dirname, 'src', 'js', 'popup.js'),
   },
   output: {
-    path: path.join(__dirname, "build"),
-    filename: "[name].bundle.js"
+    path: path.join(__dirname, 'build'),
+    filename: '[name].bundle.js',
   },
   module: {
     rules: [
       {
-        enforce: "pre",
+        enforce: 'pre',
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loader: "eslint-loader",
+        loader: 'eslint-loader',
       },
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
       },
       {
         test: /\.css$/,
         exclude: /node_modules/,
-        loader: "style-loader!css-loader",
+        loader: 'style-loader!css-loader',
       },
     ],
   },
   resolve: {
-    extensions: [".js", ".jsx", ".css"],
+    extensions: ['.js', '.jsx', '.css'],
   },
   plugins: [
     new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify(env.NODE_ENV)
+      'process.env.NODE_ENV': JSON.stringify(env.NODE_ENV),
     }),
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "popup.html"),
-      filename: "popup.html",
-      chunks: ["popup"]
+      template: path.join(__dirname, 'src', 'popup.html'),
+      filename: 'popup.html',
+      chunks: ['popup'],
     }),
     new WriteFilePlugin(),
-  ]
+  ],
 };
 
-if (env.NODE_ENV === "development") {
-  options.devtool = "cheap-module-eval-source-map";
+if (env.NODE_ENV === 'development') {
+  options.devtool = 'cheap-module-eval-source-map';
 }
 
 module.exports = options;
