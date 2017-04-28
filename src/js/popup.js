a4958959d4fc78b7177cabc5f2ee13f47d2af0f4
Evan Sangaline <esangaline@gmail.com>
2017-04-28 09:54:19 -0700

    Add a little polyfill for getting the current tab.
    This makes it easier to have a persistent window open during
    development.

--- src/js/popup.js
+++ src/js/popup.js
@@ -4,7 +4,8 @@ import { render } from 'react-dom';
 import '../css/popup.css';
 import Popup from './popup/popup';
 
-chrome.tabs.getSelected(null, (tab) => {
+const getSelected = chrome.tabs ? chrome.tabs.getSelected : (_, f) => f({ url: 'https://intoli.com' });
+getSelected(null, (tab) => {
   const activeDomain = tab.url.split('/')[2];
   const currentDomain = activeDomain.startsWith('www.') ? activeDomain.slice(4) : activeDomain;
   const initialDomain = currentDomain.indexOf('.') > -1 ? currentDomain : 'intoli.com';
